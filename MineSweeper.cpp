e7e56f79034cde5027e2e528a1835325f3577a12
yizhen05 <usukumonez@gmail.com>
2024-02-12 01:14:24 +0900

    mazu ru-ruwo rikaishiro

--- MineSweeper.cpp
+++ MineSweeper.cpp
@@ -1,18 +1,54 @@
 #include <bits/stdc++.h>
 
-// 地雷の数
+#define vertical 10
+#define horizontal 10
+#define mineNum 10
 
 using namespace std;
+int jadgeField[vertical][horizontal];
+char field[vertical][horizontal];
+// int life = 3;
+
+void isMine(int x, int y) {
+    if (jadgeField[y][x] == -1) {
+        cout << "Boom!" << endl;
+        cout << "\x1b[31mGame Over\x1b[39m" << endl;
+    } else {
+        field[y][x] = ' ';
+    }
+    // 回りの八個のそれぞれの周りの地雷の数を数える
+    int mineCount = 0;
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            if (y + i >= 0 && y + i < vertical && x + j >= 0 &&
+                x + j < horizontal) {
+                if (jadgeField[y + i][x + j] == -1) mineCount++;
+            }
+        }
+    }
+
+    // int mineCount = 0;
+    // if (y - 1 >= 0 && x - 1 >= 0) {
+    //     if (jadgeField[y - 1][x - 1] == -1) mineCount++;
+    //     if (jadgeField[y - 1][x] == -1) mineCount++;
+    //     if (jadgeField[y - 1][x + 1] == -1 && x + 1 < horizontal)
+    //     mineCount++; if (jadgeField[y][x - 1] == -1) mineCount++;
+    // }
+    // if (y + 1 < vertical && x + 1 < horizontal) {
+    //     if (jadgeField[y][x + 1] == -1) mineCount++;
+    //     if (jadgeField[y + 1][x - 1] == -1 && y + 1 < vertical) mineCount++;
+    //     if (jadgeField[y + 1][x] == -1) mineCount++;
+    //     if (jadgeField[y + 1][x + 1] == -1) mineCount++;
+    // }
+    char test = mineCount + '0';
+    cout << "test::" << test << endl;
+    if (mineCount != 0) field[y][x] = mineCount + '0';
+    cout << field[y][x] << endl;
+    // field[y][x] = char(mineCount);
+}
 
 int main(void) {
-    int vertical, horizontal;
     cout << "MineSweeper" << endl;
-    cout << "vertical horizontal>>";
-    cin >> vertical >> horizontal;
-    int mineNum = vertical * horizontal / 5;
-    // 判定用フィールド
-    int isfield[vertical][horizontal];
-    char field[vertical][horizontal];
     // 地雷の位置(minePos[地雷の固有番号][2(x,y)])
     int minePos[mineNum][2];
     // 地雷の座標をランダムに重複なく決定
@@ -35,16 +71,20 @@ int main(void) {
         minePos[i][0] = x;
         minePos[i][1] = y;
     }
-    for (int i = 0; i < mineNum; i++) {
-        isfield[minePos[i][1]][minePos[i][0]] = -1;
-    }
+
     // フィールドのつくるお
     for (int i = 0; i < vertical; i++) {
         for (int j = 0; j < horizontal; j++) {
             field[i][j] = '-';
-            isfield[i][j] = 0;
+            jadgeField[i][j] = 0;
         }
     }
+    // 地雷おくんご
+    for (int i = 0; i < mineNum; i++) {
+        jadgeField[minePos[i][1]][minePos[i][0]] = -1;
+    }
+    // デバッグ用に(2,5)に地雷を置く
+    jadgeField[2][5] = -1;
 
     // フィールドの表示
     cout << "  ";
@@ -58,4 +98,26 @@ int main(void) {
         }
         cout << endl;
     }
+
+    int k = 0;
+    while (k < 5) {
+        cout << "enter the coordinates to check>>";
+        int x, y;
+        cin >> x >> y;
+        isMine(x, y);
+        // フィールドの表示
+        cout << "  ";
+        for (int i = 0; i < horizontal; i++) cout << i << " ";
+        cout << endl;
+        for (int i = 0; i < vertical; i++) {
+            cout << i;
+            for (int j = 0; j < horizontal; j++) {
+                cout << " " << field[i][j];
+            }
+            cout << endl;
+        }
+        k++;
+    }
+
+    return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a = 8;
+    char b = a + '0';
+    cout << b << endl;
+    return 0;
+}
