847787aa8ab45430b633cac6916749b4df68573d
yizhen05 <usukumonez@gmail.com>
2024-02-12 21:16:01 +0900

    ライフ制を導入した．なぜかたまにプログラムが勝手に終了する

--- MineSweeper.cpp
+++ MineSweeper.cpp
@@ -2,14 +2,16 @@
 #include <ctime>
 #include <iostream>
 #include <vector>
-//hogehoge
+
 using namespace std;
 
 const int SIZE = 10;
-const int MINES = 10;
+const int MINENUM = 10;
+int life = 3;
 
 vector<vector<char>> board(SIZE, vector<char>(SIZE));
 vector<vector<char>> showboard(SIZE, vector<char>(SIZE));
+
 void initialize() {
     // Initialize board
     // 全部ハイフンをいれる
@@ -22,7 +24,7 @@ void initialize() {
     // 爆弾の配置
     srand(time(NULL));
     int minesPlaced = 0;
-    while (minesPlaced < MINES) {
+    while (minesPlaced < MINENUM) {
         int x = rand() % SIZE;
         int y = rand() % SIZE;
         if (board[x][y] != '*') {
@@ -42,30 +44,42 @@ void displayBoard() {
     for (int y = 0; y < SIZE; y++) {
         cout << y << " ";
         for (int x = 0; x < SIZE; x++) {
+            // cout << board[x][y] << " ";
             cout << showboard[x][y] << " ";
         }
         cout << endl;
     }
 }
-
 int countMines(int x, int y) {
     int count = 0;
     for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
             if (x + 1 >= 0 && x + 1 <= SIZE && y + 1 >= 0 && y + 1 <= SIZE) {
-                if (board[x + i][y + j] == '*') count++;
+                if (board[x + i][y + j] == '*')
+                    count++;
             }
         }
     }
     return count;
 }
-
 void open(int x, int y) {
-    int mines = countMines(x, y);
-    if (mines > 0) {
-        showboard[x][y] = '0' + mines;
+    if (showboard[x][y] != '-')
+        return;
+
+    if (board[x][y] == '*') {
+        showboard[x][y] = '*';
+        life--;
+        cout << "\033[31m BOOM!! \033[m Your life is " << life << endl;
+        return;
+    }
+    int mineNum = countMines(x, y);
+    if (mineNum > 0) {
+        // board[x][y]='0'+mines;
+        showboard[x][y] = '0' + mineNum;
     } else {
+        // board[x][y] = ' ';
         showboard[x][y] = ' ';
+
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
                 open(x + i, y + j);
@@ -74,18 +88,28 @@ void open(int x, int y) {
     }
 }
 
-int main() {
+int main(void) {
     initialize();
     displayBoard();
     int x, y;
-    do {
-        cout << "Enter x,y>>";
-        cin >> x >> y;
-        if (x < 0 || x > SIZE || y < 0 || y > SIZE) {
-            cout << "Error enter a valid value";
-        }
-    } while (x < 0 || x > SIZE || y < 0 || y > SIZE);
-    open(x, y);
-    displayBoard();
+    while (life > 0) {
+        do {
+            cout << "Enter x,y>>";
+            cin >> x >> y;
+            if (x < 0 || x > SIZE || y < 0 || y > SIZE) {
+                cout << "Error enter a valid value";
+            }
+        } while (x < 0 || x > SIZE || y < 0 || y > SIZE);
+        // if (board[x][y] == '*') {
+        //     showboard[x][y] == '*';
+        //     life--;
+        //     cout << "BOOM!! Your Life is" << life << endl;
+        //     continue;
+        // } else if (board[x][y] != '*') {
+        //     open(x, y);
+        // }
+        open(x, y);
+        displayBoard();
+    }
     return 0;
 }
