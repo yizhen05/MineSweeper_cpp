a6df06f1594a3a683b3a9fbf9d987bd21ed1cabb
yizhen05 <usukumonez@gmail.com>
2024-02-12 17:39:58 +0900

    Mine location no longer displayed

--- MineSweeper.cpp
+++ MineSweeper.cpp
@@ -9,13 +9,14 @@ const int SIZE = 10;
 const int MINES = 10;
 
 vector<vector<char>> board(SIZE, vector<char>(SIZE));
-
+vector<vector<char>> showboard(SIZE, vector<char>(SIZE));
 void initialize() {
     // Initialize board
     // 全部ハイフンをいれる
-    for (int x = 0; x < SIZE; ++x) {
-        for (int y = 0; y < SIZE; ++y) {
+    for (int x = 0; x < SIZE; x++) {
+        for (int y = 0; y < SIZE; y++) {
             board[x][y] = '-';
+            showboard[x][y] = '-';
         }
     }
     // 爆弾の配置
@@ -34,14 +35,14 @@ void initialize() {
 void displayBoard() {
     // Display board
     cout << "  ";
-    for (int x = 0; x < SIZE; ++x) {
+    for (int x = 0; x < SIZE; x++) {
         cout << x << " ";
     }
     cout << endl;
-    for (int y = 0; y < SIZE; ++y) {
+    for (int y = 0; y < SIZE; y++) {
         cout << y << " ";
-        for (int x = 0; x < SIZE; ++x) {
-            cout << board[x][y] << " ";
+        for (int x = 0; x < SIZE; x++) {
+            cout << showboard[x][y] << " ";
         }
         cout << endl;
     }
@@ -49,8 +50,8 @@ void displayBoard() {
 
 int countMines(int x, int y) {
     int count = 0;
-    for (int i = -1; i <= 1; ++i) {
-        for (int j = -1; j <= 1; ++j) {
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
             if (x + 1 >= 0 && x + 1 <= SIZE && y + 1 >= 0 && y + 1 <= SIZE) {
                 if (board[x + i][y + j] == '*') count++;
             }
@@ -62,11 +63,11 @@ int countMines(int x, int y) {
 void open(int x, int y) {
     int mines = countMines(x, y);
     if (mines > 0) {
-        board[x][y] = '0' + mines;
+        showboard[x][y] = '0' + mines;
     } else {
-        board[x][y] = ' ';
-        for (int i = -1; i <= 1; ++i) {
-            for (int j = -1; j <= 1; ++j) {
+        showboard[x][y] = ' ';
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
                 open(x + i, y + j);
             }
         }
