6aa6387f31b9ba1197cd619991ec1b2554f155ee
yizhen05 <usukumonez@gmail.com>
2024-02-20 16:59:50 +0900

    なんか動かなくなった

--- MineSweeper.cpp
+++ MineSweeper.cpp
@@ -30,7 +30,6 @@ bool isMine(int x, int y) { return (judgement_board[y] & (0x80 >> x)) == 0; }
 
 // ボードの初期化
 void initialize() {
-    // 計測開始
     for (int x = 0; x < SIZE; x++) {
         for (int y = 0; y < SIZE; y++) {
             judgement_board[y] = 0b00000000;
@@ -49,10 +48,6 @@ void initialize() {
             mines_placed++;
         }
     }
-    // 計測終了
-    // clock_t end = clock();
-    // cout << "Time: " << (double)(end - start) / CLOCKS_PER_SEC << "s" <<
-    // endl;
 }
 
 // ボードの表示
@@ -72,11 +67,6 @@ void displayBoard() {
     }
 }
 
-// ボードの範囲内かどうかを判定
-bool isInBoard(int x, int y) {
-    return x >= 0 && x < SIZE && y >= 0 && y < SIZE;
-}
-
 // 指定した座標の周囲の爆弾の数をカウント
 int countMines(int x, int y) {
     int count = 0;
@@ -84,7 +74,7 @@ int countMines(int x, int y) {
         for (int j = -1; j <= 1; j++) {
             // if (x + 1 >= 0 && x + 1 <= SIZE && y + 1 >= 0 && y + 1 <= SIZE) {
             if (isInBoard(x + i, y + j)) {
-                if (judgement_board[x + i] & (0x80 >> (y + j))) {
+                if (isMine(x + i, y + j)) {
                     count++;
                 }
             }
@@ -95,7 +85,7 @@ int countMines(int x, int y) {
 
 // セルを開く
 void openCell(int x, int y) {
-    if (!(visualized_board[x][y] == '-') && !(visualized_board[x][y] == 'F')) {
+    if (!isInBoard(x, y) || isFlagOrOpen(x, y)) {
         return;
     }
     if (judgement_board[x] & (0x80 >> y)) {
@@ -132,19 +122,19 @@ void flagOrOpen(int x, int y, int choice) {
 int main(void) {
     initialize();
     displayBoard();
-    // int x, y, choice;
-    // while (life > 0) {
-    //     do {
-    //         cout << "1. Open 2. Flag" << endl;
-    //         cout << "Enter x y Open/Flag >>";
-    //         cin >> x >> y >> choice;
-    //         if (!isInBoard(x, y) || (choice != 1 && choice != 2)) {
-    //             cout << "Error! Enter a valid value!" << endl;
-    //         }
-    //     } while (!isInBoard(x, y));
+    int x, y, choice;
+    while (life > 0) {
+        do {
+            cout << "1. Open 2. Flag" << endl;
+            cout << "Enter x y Open/Flag >>";
+            cin >> x >> y >> choice;
+            if (!isInBoard(x, y) || (choice != 1 && choice != 2)) {
+                cout << "Error! Enter a valid value!" << endl;
+            }
+        } while (!isInBoard(x, y));
 
-    //     flagOrOpen(x, y, choice);
-    //     displayBoard();
-    // }
+        flagOrOpen(x, y, choice);
+        displayBoard();
+    }
     return 0;
 }
